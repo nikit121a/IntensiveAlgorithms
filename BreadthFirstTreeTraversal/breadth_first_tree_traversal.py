11d58aa00437a43ccbc030a8c32bc30901ca32db
Nikita <nikit-@mail.ru>
2023-08-18 21:12:54 +0300

    Done

--- BreadthFirstTreeTraversal/breadth_first_tree_traversal.py
+++ BreadthFirstTreeTraversal/breadth_first_tree_traversal.py
@@ -1,19 +1,40 @@
 from typing import Dict
 from typing import List
 
+import queue
 
-def breadth_first_tree_traversal(root: int, tree: Dict[int, List[int]]) -> List[int]:
-    """
-    Completes breadth first tree traversal and returns visited vertices
-    Go to left side of tree, then right, then self
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def bfs(self, output, mark, q):
+        curr = q.get()
+        k = curr[1]
+        cur = curr[0]
+        mark.add(cur.val)
+        if len(output) <= k:
+            output.append([])
+        output[k].append(cur.val)
+        if cur.left is not None:
+            q.put([cur.left, k+1])
+        if cur.right is not None:
+            q.put([cur.right,k+1])
 
-    Args:
-        root (int): root vertex
-        tree (Dict[int, List[int]]): structure of tree
-                            (number of vertex, list of two elements: child number or None)
-    """
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        output = []
+        mark = set()
+        q = queue.Queue()
+        if root is None:
+            return []
+        q.put([root, 0])
+        while not q.empty():
+            self.bfs(output, mark, q)
+        return output
 
-    return []
+
 
 
 # https://leetcode.com/problems/binary-tree-level-order-traversal/
