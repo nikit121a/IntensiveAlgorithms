2063d7d0f2eedda2197e595726203f46ddecfac7
Nikita <nikit-@mail.ru>
2023-08-18 18:35:22 +0300

    Done

--- BubbleSort/test_bubble_sort.py
+++ BubbleSort/test_bubble_sort.py
@@ -7,5 +7,14 @@ def test_bubble_sort():
     array = []
     for i in range(5):
         array = [randint(0, 100) for j in range(10)]
-
     assert bubble_sort(array) == sorted(array)
+    array = [0]
+    assert bubble_sort(array) == sorted(array)
+    array = [-1, -2, 4]
+    assert bubble_sort(array) == sorted(array)
+    array = [-1, -1, -1]
+    assert bubble_sort(array) == sorted(array)
+    array = [2, 2, 2]
+    assert bubble_sort(array) == sorted(array)
+
+test_bubble_sort()
